7d5244162c96ba5d779924928af4e4d270b112d9
izumkineno <1465707948@qq.com>
2023-03-12 19:48:02 +0800

    静态生成文件

--- vue.config.js
+++ vue.config.js
@@ -3,13 +3,22 @@ const { defineConfig } = require('@vue/cli-service')
 const AutoImport = require('unplugin-auto-import/webpack')
 const Components = require('unplugin-vue-components/webpack')
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: './',
   productionSourceMap: false,
   configureWebpack: {
+    output: {
+      filename: 'js/[name].js',
+      chunkFilename: 'js/[name].js'
+    },
     plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].css',
+        chunkFilename: 'css/[name]css'
+      }),
       AutoImport({
         resolvers: [ElementPlusResolver()]
       }),
